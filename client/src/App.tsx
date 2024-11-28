9feb0b41579d8a762222c0fcafe065e739d7c485
Udgeet <bhatt8udgeet@gmail.com>
2024-11-28 23:53:43 +0530

    Project Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <div>
+      <Button>Har Har Mahadev!!</Button>
+    </div>
+    </>
+  )
+}
+
+export default App

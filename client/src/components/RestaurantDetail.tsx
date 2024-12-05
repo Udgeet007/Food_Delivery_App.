bafe201e62908a9b72fdbf980fa68b2b7050ef5a
Udgeet <bhatt8udgeet@gmail.com>
2024-12-06 01:01:38 +0530

    Restaurant Detail Page Done.

---
+++ client/src/components/RestaurantDetail.tsx
@@ -0,0 +1,42 @@
+import Image from "@/assets/hero_pizza.png";
+import { Badge } from "./ui/badge";
+import { Timer } from "lucide-react";
+// @ will find direct show src root folder
+const RestaurantDetail = () => {
+  return (
+    <div className="max-w-6xl mx-auto my-10 ">
+      <div className="w-full">
+        <div className="relative w-full h-32 md:h-64 lg:h-72">
+          <img
+            src={Image}
+            alt="res_image"
+            className="object-cover w-full h-full rounded-lg shadow-lg"
+          />
+        </div>
+        {/* flex-col means mobile devices we y-axis me data and x-axis flex-row medium laptop devices */}
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="my-5">
+            <h1 className="font-medium text-xl">Pizza Cave</h1>
+            <div className="flex gap-2 my-2">
+              {["Biryani", "Momos"].map((cuisine: string, idx: number) => (
+                <Badge key={idx}>{cuisine}</Badge>
+              ))}
+            </div>
+            <div className="flex md:flex-row flex-col gap-2 my-5">
+              <div className="flex items-center gap-2">
+              <Timer className="w-5 h-5"/>
+              <h1 className="flex items-center gap-2 font-medium">Delivery Time:{" "}
+                <span className="text-[#D19254]">
+                  35 mins
+                </span>
+              </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantDetail;

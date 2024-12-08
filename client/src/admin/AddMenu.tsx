e0f7471b44a0a83562a4d109a4b7bee9481ccd92
Udgeet <bhatt8udgeet@gmail.com>
2024-12-09 01:19:14 +0530

    Admin Menu Page Done.

---
+++ client/src/admin/AddMenu.tsx
@@ -0,0 +1,205 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Loader2, Plus } from "lucide-react";
+import { FormEvent, useState } from "react";
+import EditMenu from "./EditMenu";
+import { MenuFormSchema, menuSchema } from "@/schema/menuSchema";
+
+const menus = [
+  {
+    name: "Biryani",
+    description: "Udgeet Bhatt this is me This is What,",
+    price: 80,
+    image:
+      "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg ",
+  },
+  {
+    name: "Biryani",
+    description: "Udgeet Bhatt this is me This is What,",
+    price: 100,
+    image:
+      "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg",
+  },
+];
+
+const AddMenu = () => {
+  const [input, setInput] = useState<MenuFormSchema>({
+    name: "",
+    description: "",
+    price: 0,
+    image: undefined,
+  });
+  const [open, setOpen] = useState<boolean>(false);
+  const [editOpen, setEditOpen] = useState<boolean>(false);
+  const [selectedMenu, setSelectedMenu] = useState<any>();
+  const [error, setError] = useState<Partial<MenuFormSchema>>({});
+  const loading = false;
+
+  const ChangeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type } = e.target;
+    setInput({ ...input, [name]: type === "number" ? Number(value) : value });
+  };
+
+  const SubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const result = menuSchema.safeParse(input);
+    if (!result.success) {
+      const fieldErrors = result.error.formErrors.fieldErrors;
+      setError(fieldErrors as Partial<MenuFormSchema>);
+      return;
+    }
+
+    // Api Starts From Here
+  };
+  return (
+    <div className="max-w-6xl mx-auto my-10">
+      <div className="flex justify-between">
+        <h1 className="font-bold md:font-extrabold text-lg md:text-2xl">
+          Available Menus
+        </h1>{" "}
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger>
+            <Button className="bg-orange hover:bg-hoverOrange">
+              <Plus className="mr-2" />
+              Add Menus
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add A New Menu </DialogTitle>
+              <DialogDescription>
+                Create a menu that will make your restaurant stand out.
+              </DialogDescription>
+            </DialogHeader>
+            <form onSubmit={SubmitHandler} action="" className="space-y-4">
+              <div>
+                <Label>Name</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  value={input.name}
+                  onChange={ChangeEventHandler}
+                  placeholder="Enter menu name"
+                />
+                {error && (
+                  <span className="text-xs font-medium text-red-600">
+                    {error.name}
+                  </span>
+                )}
+              </div>
+              <div>
+                <Label>Description</Label>
+                <Input
+                  type="text"
+                  name="description"
+                  value={input.description}
+                  onChange={ChangeEventHandler}
+                  placeholder="Enter menu description"
+                />
+                {error && (
+                  <span className="text-xs font-medium text-red-600">
+                    {error.description}
+                  </span>
+                )}
+              </div>
+              <div>
+                <Label>Price in (Rupees)</Label>
+                <Input
+                  type="number"
+                  name="price"
+                  value={input.price}
+                  onChange={ChangeEventHandler}
+                  placeholder="Enter menu price"
+                />
+                {error && (
+                  <span className="text-xs font-medium text-red-600">
+                    {error.price}
+                  </span>
+                )}
+              </div>
+              <div>
+                <Label>Upload Menu Image</Label>
+                <Input
+                  type="file"
+                  name="image"
+                  onChange={(e) =>
+                    setInput({
+                      ...input,
+                      image: e.target.files?.[0] || undefined,
+                    })
+                  }
+                />
+                {error && (
+                  <span className="text-xs font-medium text-red-600">
+                    {error.image?.name || "Image is required"}
+                  </span>
+                )}
+              </div>
+              <DialogFooter className="mt-5">
+                {loading ? (
+                  <Button disabled className="bg-orange hover:bg-hoverOrange">
+                    <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+                    Please Wait
+                  </Button>
+                ) : (
+                  <Button className="bg-orange hover:bg-hoverOrange">
+                    Submit
+                  </Button>
+                )}
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
+      </div>
+      {menus.map((menu: any, idx: number) => (
+        <div className="mt-6 space-y-4">
+          {/* Menu Ayyega */}
+          <div className="flex flex-col md:flex-row md:items-center md:space-x-4 md:p-4 p-2 shadow-md rounded-lg border">
+            <img
+              src={menu.image}
+              alt=""
+              className="md:h-24 md:w-24 h-16 w-full object-cover rounded-lg"
+            />
+            <div className="flex-1">
+              <h1 className="text-lg font-semibold text-gray-800">
+                {menu.name}
+              </h1>
+              <p className="text-sm text-gray-600 mt-1">{menu.description}</p>
+              <h2 className="text-md font-semibold mt-2">
+                Price: <span className="text-[#D19254]">{menu.price}</span>
+              </h2>
+            </div>
+            <Button
+              onClick={() => {
+                setSelectedMenu(menu);
+                setEditOpen(true);
+              }}
+              size={"sm"}
+              className="bg-orange hover:bg-hoverOrange"
+            >
+              Edit
+            </Button>
+          </div>
+        </div>
+      ))}
+
+      <EditMenu
+        selectedMenu={selectedMenu}
+        editOpen={editOpen}
+        setEditOpen={setEditOpen}
+      />
+    </div>
+  );
+};
+
+export default AddMenu;

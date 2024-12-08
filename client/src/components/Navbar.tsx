6581839b6aad68439b8035c7d5109e4b00e8f3c0
Udgeet <bhatt8udgeet@gmail.com>
2024-12-09 02:20:05 +0530

    Admin Orders Page Done.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -196,7 +196,7 @@ const MobileNavbar = () => {
             <span>Restaurant</span>
           </Link>
           <Link
-            to="/cart"
+            to="/admin/orders"
             className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
           >
             <PackageCheck />

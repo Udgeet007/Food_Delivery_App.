25400000b96602f821e05ad2b8e9f082bcd8d0a5
Udgeet <bhatt8udgeet@gmail.com>
2024-12-14 17:53:34 +0530

    Menu Controller and Roues done.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,30 +2,34 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import userRoute from "./routes/user.route.js";
-import restaurantRoute from "./routes/restaurant.route.js"
+import restaurantRoute from "./routes/restaurant.route.js";
+import menuRoute from "./routes/menu.route.js";
+dotenv.config();
+
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-dotenv.config();
+
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //default middleware for any mernproject
-app.use(bodyParser.json({limit:'10mb'}));
-app.use(express.urlencoded({extended:true, limit:'10mb'}));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin:"http://localhost5173",
-  credentials:true
+  origin: "http://localhost5173",
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
 //api
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/restaurant", restaurantRoute);
+app.use("/api/v1/menu", menuRoute);
 //http://localhost:8000/api/v1/user/signup
 
 app.listen(PORT, () => {

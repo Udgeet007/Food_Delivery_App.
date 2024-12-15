24b4db08b3ba8be9cadc92aa7f98bc9de8e1faa6
Udgeet <bhatt8udgeet@gmail.com>
2024-12-16 02:50:13 +0530

    stripe and order function routes done.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import connectDB from "./db/connectDB.js";
 import userRoute from "./routes/user.route.js";
 import restaurantRoute from "./routes/restaurant.route.js";
 import menuRoute from "./routes/menu.route.js";
+import orderRoute from "./routes/order.route.js";
 dotenv.config();
 
 import bodyParser from "body-parser";
@@ -30,6 +31,7 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/restaurant", restaurantRoute);
 app.use("/api/v1/menu", menuRoute);
+app.use("/api/v1/order", orderRoute);
 //http://localhost:8000/api/v1/user/signup
 
 app.listen(PORT, () => {
